89d2a3ff9ef13b7d63a73996f25464b594e50d13
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2023-08-01 01:58:12 +0530

    fix: updated redirecting url for rehash logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header className="bg-white">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <a href="https://www.rehashweb3.xyz/" className="-m-1.5 p-1.5">
             <span className="sr-only">Rehash</span>
             <img className="h-8 w-auto" src="https://ik.imagekit.io/mihirsinhparmar/rehash.png" alt="Rehash Logo" />
           </a>
