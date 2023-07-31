8b5ffed4e353ac73d0dde631008039c6a6692f58
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2023-08-01 01:54:51 +0530

    fix: using cdn to serve images

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Rehash</span>
-            <img className="h-8 w-auto" src="/public/rehash.png" alt="Rehash Logo" />
+            <img className="h-8 w-auto" src="https://ik.imagekit.io/mihirsinhparmar/rehash.png" alt="Rehash Logo" />
           </a>
         </div>
       <Heading heading={'Rehash Rights Checker'} />
